6d9362845376e46fc8fdc8087635c2d0269b8c21
Sklirynsi <nikolenkosergei.work@gmail.com>
2024-10-30 21:24:55 +0700

    Add files via upload

--- 'module_1_3.py'.py
+++ 'module_1_3.py'.py
@@ -2,6 +2,7 @@ name = 'Sergei'
 print (name, type (name) )
 age = 25
 print (age, type (age) )
-new_age = + 1
-print (age + new_age, type (age + new_age))
-print ( 'is_student', type (True))
+age = age + 1
+print (age , type (age))
+is_student = True
+print (is_student)
